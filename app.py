d277265c9e55696d8533f8092451f764794f30c7
filipesenaifirjan <filipemattossenai@gmail.com>
2023-07-02 16:50:38 -0300

    finalizando

--- app.py
+++ app.py
@@ -19,17 +19,42 @@ livros = [
         'autor': 'Habitos atonicos'
     },
 ]
+
+#criar livros
+@app.route('/livros', methods=['POST'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
+
 #consultar
 @app.route('/livros', methods=['GET'])
 def obter_livros():
     return jsonify(livros)
 
-#consultar
+#consultar por id
 @app.route('/livros/<int:id>', methods=['GET'])
 def obter_livro_por_id(id):
     for livro in livros:
       if  livro.get('id')  == id:
             return jsonify(livro)   
+#editar
+@app.route('/livros/<int:id>', methods=['PUT'])
+def editar_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            livro[indice].update(livro_alterado)
+            return jsonify(livros[indice])
+
+@app.route('/livros/<int:id>', methods=['DELETE'])        
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+    return jsonify(livros)                
 
 
 app.run(port=5000, host='localhost' ,debug=True)    
